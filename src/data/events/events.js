5c7007abb4175f88a2ff44253f9390462ec9e101
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-09-26 16:21:56 +0200

    Change the ISO timestamps to be in CET/CEST

--- src/data/events/events.js
+++ src/data/events/events.js
@@ -5,37 +5,37 @@ const eventLogos = Object.fromEntries(
 const events = [
     {
         name: "RAVE",
-        startDate: new Date("2024-10-05T23:00:00Z"),
+        startDate: new Date("2024-10-06T01:00:00+02:00"),
         poster: eventLogos.rave,
         club: "Club Orion",
     },
     {
         name: "Midmonth Madness",
-        startDate: new Date("2024-10-26T20:00:00Z"),
+        startDate: new Date("2024-10-26T22:00:00+02:00"),
         poster: eventLogos.midmonthMadness,
         club: "Club Orion",
     },
     {
         name: "Quest Rave",
-        startDate: new Date("2024-10-26T23:00:00Z"),
+        startDate: new Date("2024-10-27T01:00:00+02:00"),
         poster: eventLogos.questRave,
         club: "Club SHATAXE",
     },
     {
         name: "Halloween Party",
-        startDate: new Date("2024-10-31T21:30:00Z"),
+        startDate: new Date("2024-10-31T22:30:00+01:00"),
         poster: eventLogos.halloween,
         club: "Club Orion",
     },
     {
         name: "RAVE",
-        startDate: new Date("2024-11-03T00:00:00Z"),
+        startDate: new Date("2024-11-03T01:00:00+01:00"),
         poster: eventLogos.rave,
         club: "Club Orion",
     },
     {
         name: "The Land of Christmas",
-        startDate: new Date("2024-12-24T22:00:00Z"),
+        startDate: new Date("2024-12-24T23:00:00+01:00"),
         poster: eventLogos.christmas,
         club: "Club Orion",
     },
