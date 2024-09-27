4f58b62c2a449c77e4c066e9895b8e6f6fbd632d
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-09-27 23:34:07 +0200

    Add new Event pictures

--- src/data/events/events.js
+++ src/data/events/events.js
@@ -6,13 +6,13 @@ const events = [
     {
         name: "RAVE",
         startDate: new Date("2024-10-06T01:00:00+02:00"),
-        poster: eventLogos.rave,
+        poster: eventLogos['RAVE-Horizontal-GMT2'],
         club: "Club Orion",
     },
     {
         name: "Midmonth Madness",
         startDate: new Date("2024-10-26T22:00:00+02:00"),
-        poster: eventLogos.midmonthMadness,
+        poster: eventLogos['MMM-Horizontal-GMT2'],
         club: "Club Orion",
     },
     {
@@ -30,7 +30,7 @@ const events = [
     {
         name: "RAVE",
         startDate: new Date("2024-11-03T01:00:00+01:00"),
-        poster: eventLogos.rave,
+        poster: eventLogos['RAVE-Horizontal-GMT1'],
         club: "Club Orion",
     },
     {
