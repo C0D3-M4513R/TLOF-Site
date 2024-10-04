1059096e2546e5d5ddf76b81068ace728050df17
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-10-04 21:36:51 +0200

    Update the Homepage Text

--- src/components/home/sections/About.vue
+++ src/components/home/sections/About.vue
@@ -14,10 +14,12 @@ export default {
             The Land of Future does Parties in VRChat. We usually are in Club Orion and play Psytrance, Bigroom or Harstyle.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cumque ipsum, nemo quibusdam temporibus vitae voluptates! Eius et fuga laudantium minus molestias optio rerum voluptates. Asperiores debitis dolor, dolore dolores eaque explicabo facilis illum iure labore minus porro quod sed totam! Ad aliquam aspernatur assumenda dolor, doloremque eius facere iusto, molestias placeat quam quisquam similique sit tempore, totam veniam voluptatem voluptatum. Debitis maxime nam, nemo qui unde voluptatum. Distinctio ea earum harum, itaque modi praesentium similique ut! Accusamus adipisci alias aliquam cupiditate dicta dolor dolorem ducimus et excepturi fugit nobis numquam odio pariatur, reprehenderit rerum sed tempore ut voluptate voluptatibus.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet animi aspernatur assumenda autem corporis cumque cupiditate, dicta dignissimos exercitationem explicabo facilis fugiat laudantium magni molestias neque non nostrum numquam perspiciatis quasi quis quos reiciendis similique sint suscipit temporibus tenetur vitae voluptate voluptates voluptatum. Assumenda ducimus excepturi maiores suscipit vel.
+            The Land of Future started 2017 and has been active since today, except for a short break at the start of 2023.
+            In the Start Angelshooter made Playlists for people to listen to.
+            Over the time that evolved into mixing the tracks together.
+            Eventually those Mixes became longer and better.
+            During that time Angelshooter hosted Parties in VRChat.
+            Over the Years the Parties have become more successful (within the usual fluctuations).
           </p>
         </div>
       </div>
@@ -26,33 +28,6 @@ export default {
 </template>
 
 <style scoped>
-#magma {
-  color: #970000;
-}
-#magma:hover {
-  color: #ab0000;
-}
-
-@media (prefers-color-scheme: dark) {
-  #magma {
-    color: #ffb300;
-  }
-  #magma:hover {
-    color: #ffcb00;
-  }
-}
-
-#team {
-  color: var(--color-link-secondary)
-}
-#team:hover {
-  color: var(--color-link-secondary-hover)
-}
-
-#magma, #team {
-  text-decoration: underline;
-}
-
 strong {
   font-weight: bold;
 }
@@ -64,68 +39,16 @@ strong {
   justify-content: center;
 }
 
-#calculation {
-  margin-top: 35px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  user-select: none;
-}
-
-#sum {
-  display: flex;
-  align-items: center;
-}
-
-#calculation img {
-  width: 150px;
-  height: 150px;
-  object-fit: contain;
-
-  background-color: var(--color-background-mute);
-
-  border-radius: 10px;
-  box-shadow: 0 0 10px var(--color-border);
-  margin: 0 20px;
-  padding: 10px;
-}
-#calculation img[alt="Ketting"] {
-  padding: 20px; /* Give the Ketting logo a bit more padding */
-}
-
-#plus-icon,
-#equals-icon {
-  font-size: 4em;
-  margin: 0 40px;
-}
-
 #text {
   margin: 40px 0;
 }
 
-p:not(:last-child) {
-  margin-bottom: 20px;
+.content {
+  max-width: 80rem;
+  margin: 0 auto;
 }
 
-@media (max-width: 768px) {
-  #sum {
-    flex-direction: column;
-    align-items: center;
-  }
-
-  #calculation img {
-    width: 100px;
-    height: 100px;
-  }
-
-  #calculation img[alt="Ketting"] {
-    padding: 10px;
-  }
-
-  #plus-icon,
-  #equals-icon {
-    font-size: 2em;
-    margin: 0 10px;
-  }
+p:not(:last-child) {
+  margin-bottom: 20px;
 }
 </style>
