a056968099e22bbe4477b21d4617753dbf9bf233
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-10-08 15:11:53 +0200

    rework Event page to include Poster

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -58,6 +58,11 @@ export default {
       if (isServer) date += " UTC";
       return date;
     },
+    posterArray(){
+      if (Array.isArray(this.event.poster)) return this.event.poster;
+      if (typeof this.event.poster === 'string') return [this.event.poster];
+      return [];
+    }
   },
   components: {
     Event,
@@ -68,11 +73,12 @@ export default {
 <template>
   <div id="wrapper">
     <div class="heading">
-      <div>
+      <div class="description">
         <h1>Images for {{event.name}} on {{localEventDate}}</h1>
         <h2 v-if="event.club">at {{event.club}}</h2>
         <h3 v-if="event.description">{{event.description}}</h3>
       </div>
+      <img class="poster" v-for="poster in posterArray" :src="poster" alt="Event Poster">
     </div>
     <div class="contents">
       <img v-for="image in images" :src="image" alt="Event Image">
@@ -82,9 +88,17 @@ export default {
 
 <style scoped>
 .heading {
+  width: 80%;
   display: flex;
-  align-items: center;
-  flex-direction: column;
+  justify-content: flex-end;
+  flex-direction: row;
+  margin: 0 auto 1em;
+}
+.description {
+  flex-grow: 1;
+}
+.poster {
+  height: 10em;
 }
 .contents {
   display: flex;
